c53b1d5e229759263317e79347cb7f0c3c340326
Uliana <unadorozhna1@gmail.com>
2023-03-15 13:30:23 +0200

    dogs are moving

--- js/main.js
+++ js/main.js
@@ -13,34 +13,84 @@ const dogRender = () => {
         //console.log(x, y)
         let htmlDog = `<div class="htmlDog" style="top:${y}px; left:${x}px" >${dog.name}</div>`;
 
-        console.log(dog.name, x, y)
+        //console.log(dog.name, x, y)
     
         return htmlDog;
     });
 
     const newDogs = allDogs.join('');
 
+
     document.querySelector('.container').innerHTML = newDogs;
+
+
+
+    const dogItems = [] = document.querySelectorAll('.htmlDog');
+    dogItems.forEach((dog) => {
+        dog.addEventListener('click', (event) => {
+            const x = event.pageX - dog.offsetLeft;
+            const y = event.pageY - dog.offsetTop;
+            //console.log(x +':'+ y )
+
+
+            const getCoordLeft = dog.getBoundingClientRect();
+            const getCoordTop = dog.getBoundingClientRect();
+            //console.log(getCoordLeft, getCoordTop)
+
+
+            if(x <= 25  && y <= 25){
+                console.log('left top')
+                //dog.style.left = getCoordLeft - '5 px';
+                dog.style.left = dog.offsetLeft - 5 + 'px';
+                dog.style.top = dog.offsetTop - 5 + 'px';
+               
+            }
+            if(x >= 25  && y >=25){
+                console.log('right bottom')
+                dog.style.left = dog.offsetLeft + 5 + 'px';
+                dog.style.top = dog.offsetTop + 5 + 'px';
+            }
+            if(x >= 25  && y <=25){
+                console.log('right top')
+                dog.style.left = dog.offsetLeft + 5 + 'px';
+                dog.style.top = dog.offsetTop - 5 + 'px';
+            }
+            if(x <= 25  && y >=25){
+                console.log('left bottom')
+                dog.style.left = dog.offsetLeft - 5 + 'px';
+                dog.style.top = dog.offsetTop + 5 + 'px';
+            }
+        })
+    })
+
 }
 
 document.querySelector('.start').addEventListener('click', dogRender);
 
 
+
 //click position
-document.querySelector('.container').addEventListener('click', event =>{
-    //console.log("X:", event.clientX,"Y:", event.clientY);
+// document.querySelector('.container').addEventListener('click', event =>{
+//     //console.log("X:", event.clientX,"Y:", event.clientY);
 
-    const dogItem = [] = document.querySelectorAll('.htmlDog');
+//     const dogItem = [] = document.querySelectorAll('.htmlDog');
+//     //console.log(dogItem)
 
-    dogItem.forEach((dog) =>{
-        const getCoordLeftX = dog.getBoundingClientRect().x;
-        const getCoordTopY = dog.getBoundingClientRect().y;
-        //console.log(getCoordLeft, getCoordTop)
+//     dogItem.forEach((dog) =>{
+//         const getCoordLeftX = dog.getBoundingClientRect().x;
+//         const getCoordTopY = dog.getBoundingClientRect().y;
+//         //console.log(getCoordLeft, getCoordTop)
+//         const getWidth = dog.getBoundingClientRect().width;
+//         const getHeight = dog.getBoundingClientRect().height;
+//         //console.log(getWidth, getHeight)
 
-        if(event.clientX >= getCoordLeftX){
-            console.log('+')
-        }
-    })
+//         // if(event.clientX == getCoordLeftX || 
+//         //     event.clientY == getCoordTopY){
+//         //     console.log('+')
+//         // }
+
+//         console.log((event.pageX - dog.offsetLeft) + ':' + (event.pageY - dog.offsetTop))
+//     })
     
 
-})
+// })
